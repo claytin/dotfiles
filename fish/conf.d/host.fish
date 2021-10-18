423189c065f78fc623ba4efc5e351bf41a9faa8e
claytin <>
2021-10-17 21:50:50 -0300

    Proper configuration of HOST

--- fish/conf.d/host.fish
+++ fish/conf.d/host.fish
@@ -1,5 +1,5 @@
 if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname | cut -d . -f 1)
+    set -g __fish_prompt_hostname (hostname)
 end
 
-set -x HOST __fish_prompt_hostname
+set -x HOST $__fish_prompt_hostname
