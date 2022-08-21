b7ea7302d1e28d920ba31ab47cbbf592b57fefc4
claytin <>
2022-08-21 14:29:05 -0300

    No more right prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,7 +1,1 @@
-function fish_right_prompt
-    # print only the basename, and if in $HOME only its alias '~'
-    set -l cwd (basename (pwd) | string replace (basename $HOME) '~')
-
-    test $cwd != '~' && set cwd '.../'$cwd
-    cecho --under $cwd
-end
+function fish_right_prompt; end
