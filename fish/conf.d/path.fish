714c515720cdc94c4d4699349989344d8541876e
claytin <>
2023-09-30 21:59:07 -0300

    Added Rust binaries to the path

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,7 +1,10 @@
-## APL
+# APL
 set LOCAL_PATH "/opt/apl-1.6/bin"
 
-## rtorrent magnet script
+# rtorrent magnet script
 set LOCAL_PATH $LOCAL_PATH "/opt/magnet"
+
+# Rust
+set LOCAL_PATH $LOCAL_PATH "$HOME/.cargo/bin"
 
 set -x PATH $PATH $LOCAL_PATH
