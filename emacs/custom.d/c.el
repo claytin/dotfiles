59d582514e8f5d42335672c54923a4ab6adedf8e
claytin <>
2025-10-22 06:43:53 -0300

    Slight changes to how indent-tabs-mode is enabled

--- emacs/custom.d/c.el
+++ emacs/custom.d/c.el
@@ -1,4 +1,9 @@
 ;; C/C++ mode options
 (setq c-default-style "stroustrup"
-      c-basic-offset tab-width
-      indent-tabs-mode t)
+      c-basic-offset tab-width)
+
+(add-hook 'c-mode-hook
+          (lambda () (setq indent-tabs-mode t)))
+
+(add-hook 'c++-mode-hook
+          (lambda () (setq indent-tabs-mode t)))
