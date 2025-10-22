f655f489ca6159cde1cd10dba37c8c277919f795
claytin <>
2025-10-22 07:09:09 -0300

    Better looking

--- emacs/custom.d/ido.el
+++ emacs/custom.d/ido.el
@@ -1,11 +1,14 @@
+;; (I)nteractive DO
+
+;; Enable ido as defautl
+(require 'ido)
+(ido-mode t)
+
+;; Basic options
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-(ido-mode 1)
 
-;;(if (< emacs-major-version 25)
-;;	(setq ido-separator "\n")
-;;  (setf (nth 2 ido-decorations) "\n"))
-
+;; Extensions to igone when matching -------------------------------------------
 (setq completion-ignored-extensions
 	  (append completion-ignored-extensions
 			  '(".zip" ".rar" ".xz" ".gz" ".bzip"
@@ -16,8 +19,9 @@
 				".djvu" ".tiff" ".bmp" ".gif" ".jpg" ".jpeg" ".png" ".webp")))
 
 (setq ido-ignore-extensions t)
+;; Extension handling ends here ------------------------------------------------
 
-;; Do not ask for confirmation when trying to edit an unexistent buffer, just
-;; do it!
+;; Do not ask for confirmation when trying to edit an unexistent buffer.
+;; Just do it!
 (setq ido-create-new-buffer 'always)
 (setq-default confirm-nonexistent-file-or-buffer nil)
